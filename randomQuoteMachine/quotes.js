7a92ad73be7e036bc72e9fbeb3d182e52a57c23b
samma1997 <57420198+samma1997@users.noreply.github.com>
2022-08-22 00:16:36 +0200

    Add files via upload

---
+++ randomQuoteMachine/quotes.js
@@ -0,0 +1,33 @@
+const QUOTEBANK = [
+{
+  quote:  "Quando qualcosa è abbastanza importante, lo fai anche se le probabilità non sono a tuo favore.",
+  author: "Elon Musk"
+},{
+  quote: "Ad alcune persone non piace il cambiamento, ma è necessario abbracciare il cambiamento se l'alternativa è il disastro.",
+  author: "Elon Musk"
+},{
+  quote:" La pazienza è una virtù e sto imparando la pazienza. È una lezione dura.",
+  author: "Elon Musk"
+},{
+  quote: "Preferirei essere ottimista e sbagliato piuttosto che pessimista e giusto.",
+  author: "Elon Musk"
+}, {
+  quote: "Le persone lavorano meglio quando sanno qual è l'obiettivo e perché. È importante che le persone non vedano l'ora di venire al lavoro al mattino e si divertano a lavorare.",
+  author: "Elon Musk"
+}, {
+  quote:"I razzi sono fantastici. Non c'è niente da fare.",
+  author: "Elon Musk"
+}, {
+  quote: "Non sarò mai felice senza avere qualcuno. Andare a dormire da solo mi uccide.",
+  author: "Elon Musk"
+}, {
+  quote: "Quando ero al college, volevo essere coinvolto in cose che avrebbero cambiato il mondo.",
+  author: "Elon Musk"
+}, {
+  quote: "Qualsiasi prodotto che necessita di un manuale per funzionare è rotto.",
+  author: "Elon Musk"
+}, {
+  quote: "In realtà, l'unica cosa che ha senso è lottare per una maggiore illuminazione collettiva.",
+  author: "Elon Musk"
+}
+];
